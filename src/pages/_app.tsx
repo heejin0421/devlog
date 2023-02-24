65491d49fd94d299b095db622e662cbbbe5889cf
RohHeeJin <ranin00421@gmail.com>
2023-02-25 04:31:21 +0900

    🎨 metadata 재수정

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,14 @@
 import '@/styles/globals.css';
 import '@/styles/fonts.css';
 import type { AppProps } from 'next/app';
+import { DefaultSeo } from 'next-seo';
+import SEO from '../next-seo.config';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <DefaultSeo {...SEO} />
+      <Component {...pageProps} />
+    </>
+  );
 }
