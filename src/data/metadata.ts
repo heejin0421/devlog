b20617f46bb2b4a64ea439f15cd0d91057a41576
RohHeeJin <ranin00421@gmail.com>
2023-02-25 04:21:51 +0900

    📝 metadata 수정

--- src/data/metadata.ts
+++ src/data/metadata.ts
@@ -1,5 +1,9 @@
 const metadata = {
-  title: 'My Blog',
+  meta: {
+    url: 'https://devlog-khaki.vercel.app/',
+    title: '안녕하세요. 노희진입니다.',
+    description: '배우고 기록하여 익히는 공간입니다.',
+  },
 };
 
 export default metadata;
