1ebe913ba8cac2001270e4c8ffeb4075e8db0f91
RohHeeJin <ranin00421@gmail.com>
2023-02-25 15:39:34 +0900

    📝 블로그 소개글 변경

--- src/data/metadata.ts
+++ src/data/metadata.ts
@@ -1,7 +1,7 @@
 const metadata = {
   meta: {
     url: 'https://devlog-khaki.vercel.app/',
-    title: 'Heejin | Frontend Developer',
+    title: '노희진 기술 블로그',
     description: '배우고 기록하여 익히는 공간입니다.',
   },
 };
